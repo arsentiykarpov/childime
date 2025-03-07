02ee2562e858428d096e0bf4e6b67104f882cb90
Arsentii Karpov <arsengizer@yandex.ru>
2025-03-07 16:39:43 +0700

    working simple dummy compose login screen

--- app/src/main/java/cloud/karpov/MainActivity.kt
+++ app/src/main/java/cloud/karpov/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cloud.karpov.ui.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
