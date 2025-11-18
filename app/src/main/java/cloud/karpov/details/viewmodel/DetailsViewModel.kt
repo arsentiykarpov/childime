843b6fd21ae23ba07f65736f23f521a94f0f71f2
Arsentii Karpov <arsengizer@yandex.ru>
2025-11-18 20:43:54 +0300

    add details dummy screen

---
+++ app/src/main/java/cloud/karpov/details/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,48 @@
+package cloud.karpov.details.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import cloud.karpov.ai.repository.AiRepository
+import cloud.karpov.details.usecase.DetailsAction
+import cloud.karpov.details.usecase.DetailsViewState
+import cloud.karpov.details.usecase.InitialUseCase
+import cloud.karpov.mvi.BaseViewModel
+import cloud.karpov.mvi.MviUseCase
+
+class DetailsViewModel constructor(private val aiRepository: AiRepository, val context: Context) :
+    BaseViewModel<MviUseCase<DetailsAction, DetailsViewState>, DetailsViewState, DetailsAction>() {
+
+    init {
+        start()
+    }
+
+    override fun bindActions() {
+        bindAction(DetailsAction.InitDetailsAction::class, InitialUseCase())
+    }
+
+    override fun getInitialViewState(): DetailsViewState {
+        return DetailsViewState.Loading
+    }
+
+    override fun getInititalAction(): DetailsAction {
+        return DetailsAction.InitDetailsAction()
+    }
+
+    override fun reduceViewState(
+        fullViewState: DetailsViewState,
+        partialViewState: DetailsViewState
+    ): DetailsViewState {
+        return partialViewState
+    }
+}
+
+class DetailsViewModelFactory(private val repo: AiRepository, val context: Context) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
+            return DetailsViewModel(repo, context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
